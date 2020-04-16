9a27d5cae40e637654220988c6f4df03edc81a26
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-04-16 15:44:31 +0200

    Implement Board class

---
+++ src/main/java.ch.heigvd.gen.monopoly/Board.java
@@ -0,0 +1,24 @@
+package java.ch.heigvd.gen.monopoly;
+
+public class Board {
+  private static int NUMBER_OF_SQUARES = 40;
+  private static String SQUARE_NAME = "Square #";
+
+  private Square[] squares;
+
+  public Board() {
+    squares = new Square[NUMBER_OF_SQUARES];
+    for (int i = 0; i < NUMBER_OF_SQUARES; i++) {
+      squares[i] = new Square(SQUARE_NAME + i);
+    }
+  }
+
+  public Square getSquare(int i) {
+    if (0 < i  || i > NUMBER_OF_SQUARES) {
+      throw new IndexOutOfBoundsException("There are only " + NUMBER_OF_SQUARES +
+          " on a monopoly board.");
+    }
+
+    return squares[i];
+  }
+}
