5895e81cfbb1dc3f9e86e89f165c264cdfdc26e2
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-04-16 16:18:23 +0200

    Added javadoc to Board

--- src/main/java/ch/heigvd/gen/monopoly/Board.java
+++ src/main/java/ch/heigvd/gen/monopoly/Board.java
@@ -6,6 +6,11 @@ public class Board {
 
   private Square[] squares;
 
+  /**
+   * Board constructor.
+   *
+   * <p>Will initialize an array of 40 Squares</p>
+   */
   public Board() {
     squares = new Square[NUMBER_OF_SQUARES];
     for (int i = 0; i < NUMBER_OF_SQUARES; i++) {
@@ -13,10 +18,15 @@ public class Board {
     }
   }
 
+  /**
+   * Get the square at given index.
+   * @param i The index corresponding to the wanted square
+   * @return The square at index i
+   */
   public Square getSquare(int i) {
     if (0 < i  || i > NUMBER_OF_SQUARES) {
-      throw new IndexOutOfBoundsException("There are only " + NUMBER_OF_SQUARES +
-          " on a monopoly board.");
+      throw new IndexOutOfBoundsException("There are only " + NUMBER_OF_SQUARES
+          + " on a monopoly board.");
     }
 
     return squares[i];
