b78dc2849ba28e756b9f2b4b40cb00b9f5007183
Alexandre Piveteau <alexandre@piveteau.email>
2020-04-17 09:35:02 +0200

    Implement the takeTurn method

--- src/main/java/ch/heigvd/gen/monopoly/Player.java
+++ src/main/java/ch/heigvd/gen/monopoly/Player.java
@@ -2,7 +2,22 @@ package ch.heigvd.gen.monopoly;
 
 public class Player {
 
-  public void takeTurn(Die[] dice, Board board) {
+  private Piece piece;
 
+  /**
+   * Lets a {@link Player} take a turn, and move their own {@link Piece}.
+   *
+   * @param dice  The {@link Die} array that the player should use.
+   * @param board The {@link Board} on which actions take place.
+   */
+  public void takeTurn(Die[] dice, Board board) {
+    int sum = 0;
+    for (Die die : dice) {
+      die.roll();
+      sum += die.getFaceValue();
+    }
+    Square oldLoc = piece.getLocation();
+    Square newLoc = board.getSquare(oldLoc, sum);
+    piece.setLocation(newLoc);
   }
 }
