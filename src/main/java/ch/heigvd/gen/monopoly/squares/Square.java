c230c462ae18752d85ecec704a5dc7d22ca9fc9f
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-04-21 08:55:56 +0200

    Add comment about abstract method

--- src/main/java/ch/heigvd/gen/monopoly/squares/Square.java
+++ src/main/java/ch/heigvd/gen/monopoly/squares/Square.java
@@ -3,6 +3,7 @@ package ch.heigvd.gen.monopoly.squares;
 import ch.heigvd.gen.monopoly.Player;
 
 public abstract class Square {
+
   private String name;
 
   protected Square(String name) {
@@ -13,6 +14,11 @@ public abstract class Square {
     return name;
   }
 
+  /*
+   * This method is not abstract because we'd need to call the sysout on every implementation.
+   * It's more logical to set it here and still override it when needed. The class is still
+   * abstract.
+   */
   public void landedOn(Player p) {
     System.out.println(p.getName() + " has landed on square " + name);
   }
